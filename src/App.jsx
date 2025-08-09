3420b97c7f4cfb69a0529814ff675fe83c395197
Arnab Paul <arnab2181@gmail.com>
2025-08-09 19:11:32 +0530

    Invoices Page is done (Initial UI Design)

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import WelcomeMessage from "./Authentication/Welcome/WelcomeMessage";
 import LoginPage from "./Authentication/Login/LoginPage";
 import RegisterPage from "./Authentication/Register/RegisterPage";
 import Navbar from "./Dashboard/Navigation/Navbar";
+import Invoices from "./Invoices/Invoices";
 import "./App.css";
 
 function App() {
@@ -10,7 +11,7 @@ function App() {
     <>
       <Router>
         <Routes>
-          <Route path="/" element={<Navbar />} />   // Home route with Navbar - will change based on the FireBase integration , mainly Authentication and Cloud Firestore
+          <Route path="/" element={<Invoices />} />   // Home route with Navbar - will change based on the FireBase integration , mainly Authentication and Cloud Firestore
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
           
